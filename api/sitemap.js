f4fc532108a152e0183424347449c71c970660f6
pchroonic <145560895+pchroonic@users.noreply.github.com>
2026-09-12 19:23:50 +0100

    Index only live Namdar services

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,2 +1,14 @@
 const {db,safeError}=require('../lib/server');
-module.exports=async function handler(req,res){try{if(req.method!=='GET'){res.statusCode=405;return res.end('Method not allowed')}const jobs=await db('portfolio_jobs?published=eq.true&select=id,completed_at&order=completed_at.desc.nullslast&limit=500').catch(()=>[]);const urls=['','/services/window-cleaning','/services/gutter-cleaning','/services/roof-cleaning','/services/jet-washing','/services/handyman','/services/3d-property-tours','/areas/london','/areas/south-london','/areas/lewisham','/work','/privacy','/terms','/cookies'];for(const j of jobs||[])urls.push('/work/'+encodeURIComponent(j.id));const body='<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'+urls.map(u=>'  <url><loc>https://namdar.co.uk'+u+'</loc></url>').join('\n')+'\n</urlset>';res.statusCode=200;res.setHeader('Content-Type','application/xml; charset=utf-8');res.setHeader('Cache-Control','public, max-age=300, s-maxage=1800');res.end(body)}catch(e){return safeError(res,e)}};
+const {loadServiceCatalog}=require('../lib/service-catalog');
+module.exports=async function handler(req,res){try{
+  if(req.method!=='GET'){res.statusCode=405;return res.end('Method not allowed')}
+  const [services,jobs]=await Promise.all([
+    loadServiceCatalog(db),
+    db('portfolio_jobs?published=eq.true&select=id,service_key,completed_at&order=completed_at.desc.nullslast&limit=500').catch(()=>[])
+  ]);
+  const live=services.filter(s=>s.status==='live'),liveKeys=new Set(live.map(s=>s.service_key));
+  const urls=['',...live.map(s=>`/services/${s.slug}`),'/areas/london','/areas/south-london','/areas/lewisham','/work','/privacy','/terms','/cookies'];
+  for(const j of jobs||[])if(liveKeys.has(j.service_key))urls.push('/work/'+encodeURIComponent(j.id));
+  const body='<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'+urls.map(u=>'  <url><loc>https://namdar.co.uk'+u+'</loc></url>').join('\n')+'\n</urlset>';
+  res.statusCode=200;res.setHeader('Content-Type','application/xml; charset=utf-8');res.setHeader('Cache-Control','public, max-age=120, s-maxage=300');res.end(body)
+}catch(e){return safeError(res,e)}};
