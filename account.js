1ff1e1914d7b3c75af62523e66eb624241a8669c
pchroonic <145560895+pchroonic@users.noreply.github.com>
2026-09-12 19:23:29 +0100

    Load customer service availability controls

--- account.js
+++ account.js
@@ -1,7 +1,8 @@
 (()=>{
-  const v='6.4.16-auth-captcha-1';
+  const v='6.4.17-service-stages-1';
   document.write(`<script src="/account-original.js?v=${v}"><\/script>`);
   document.write(`<script src="/account-captcha-guard.js?v=${v}"><\/script>`);
   document.write(`<script src="/account-auth-hotfix.js?v=${v}"><\/script>`);
   document.write(`<script src="/account-mfa-guard.js?v=${v}"><\/script>`);
+  document.write(`<script src="/account-service-availability.js?v=${v}"><\/script>`);
 })();
